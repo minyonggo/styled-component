75b3fe2ed69a00cbc0b7e327a5c91979eaeb494e
Minyong#2020.06.15 <ricky23@yonsei.ac.kr>
2020-06-15 11:57:25 +0900

    #6. Theming

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import styled, {createGlobalStyle, keyframes, css} from 'styled-components';
+import styled, {createGlobalStyle, keyframes, css, ThemeProvider} from 'styled-components';
+import theme from './theme';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -7,53 +8,29 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 25px 15px;
+  background-color: ${props => props.theme.successColor};
+`;
+
 class App extends Component {
   render() {
     return (
-      <Container>
-        <GlobalStyle />
-        <Button rotationTime={5}>Hello!</Button>
-        <Button danger rotationTime={5}>Hello!</Button>
-        <Anchor href="https://google.com">Go to Google</Anchor>
-      </Container>
+      <ThemeProvider theme={theme}>
+        <Container>
+          <GlobalStyle />
+          <Button />
+        </Container>
+      </ThemeProvider>
     );
   }
 }
-
-const rotation = keyframes`
-  from { transform: rotate(0deg); }
-  to   { transform: rotate(360deg);}
-`;
-const rotation_css = (danger, rotationTime) => {
-  if(danger) return css`${rotation} ${rotationTime}s linear infinite;`;
-  return css`${rotation} ${rotationTime}s linear infinite reverse;`
-};
-
 
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: pink;
-`;
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: white;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus {
-    outline: none;
-  }
-  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-  animation: ${props => (rotation_css(props.danger, props.rotationTime))};
-`;
-
-const Anchor = styled(Button.withComponent("a"))`
-  text-decoration: none;
 `;
 
 export default App;
